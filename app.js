fd71014ddc8fd8110642f682bc3f1f10515219c4
khanfaizan98559 <khanfaizan98559@gmail.com>
2025-04-06 22:35:04 +0530

    major changes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const ejs= require('ejs');
+const path= require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("ejs", ejsMate);
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
